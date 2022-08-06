a56e4bcf5701e0d9040135fa223c0f72b5b3cec4
Yiming Wang <wangym5106@gmail.com>
2022-08-06 22:52:05 +0800

    strip empty strings

--- generate_zh_msg.py
+++ generate_zh_msg.py
@@ -1,11 +1,9 @@
 import pickle
 import json
 
-dust514_ids = json.load(open('dust514_ids.json', 'r'))
-
-en_msgs = pickle.load(open('localization_fsd_en-us.pickle', 'rb'))
+en_msgs = json.load(open('translation-en.json', 'r', encoding='utf-8'))
 zh_msgs = pickle.load(open('localization_fsd_zh.pickle', 'rb'))
 
-striped_msg = {k: v[0] for k, v in zh_msgs[1].items() if k in en_msgs[1] and k not in dust514_ids}
+striped_msg = {k: v[0] for k, v in zh_msgs[1].items() if str(k) in en_msgs}
 
 json.dump(striped_msg, open('translation-zh_Hans.json', 'w', encoding='utf-8'), ensure_ascii=False, sort_keys=True, indent=0)
